1d660ce733ef6eef8f7335614d92ec4b40e8d5c8
moros101 <mohitgoswami16@gmail.com>
2025-10-18 01:59:14 +0530

    single number

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,3 +3,8 @@ const { add } = require('../src/stringCalculator');
 test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number itself when one number is provided', () => {
+    expect(add("1")).toBe(1);
+  });
+  
