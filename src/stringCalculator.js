425547383479795b1cf1af249222335e2ffcaad0
moros101 <mohitgoswami16@gmail.com>
2025-10-18 02:13:06 +0530

    handle custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,13 @@
 function add(numbers) {
     if (!numbers) return 0;
-    const nums = numbers.split(/[\n,]/).map(Number);
+
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0][2]); // e.g. ;
+      numbers = parts[1];
+    }
+    const nums = numbers.split(delimiter).map(Number);
 
     return nums.reduce((a, b) => a + b, 0);
 }
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -15,6 +15,11 @@ test('multiple comma separated numbers', () => {
 test('handles new lines', () => {
     expect(add("1\n2,3")).toBe(6);
 });
+
+test('custom delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
+  
   
   
   
