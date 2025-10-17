31278e7abbd38aaca97f1f05894207d5203e36f0
moros101 <mohitgoswami16@gmail.com>
2025-10-18 02:00:53 +0530

    export missing fix

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,5 +2,8 @@ function add(numbers) {
     if (!numbers) return 0;
     const nums = numbers.split(',').map(Number);
     return nums.reduce((a, b) => a + b, 0);
-  }
+}
+  
+  
+  module.exports = { add };
   
